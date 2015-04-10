a8ab2e2106eaf7d4f0edf7dd2afc591396612c41
Cal Evans <cal@calevans.com>
2015-04-09 19:11:38 -0500

    Added Recursive command back in.

---
+++ app/Iterators/Command/RecursiveCommand.php
@@ -0,0 +1,43 @@
+<?PHP
+namespace Iterators\Command;
+ 
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface as InputInterface;
+use Symfony\Component\Console\Output\OutputInterface as OutputInterface;
+use Iterators\Classes\SeekableIterator;
+
+class RecursiveCommand extends Command
+{
+
+    protected function configure()
+    {
+
+        $this->setName("recursive")
+             ->setDescription("Demonstrate a Recrusive Iterator")
+             ->setHelp("Iterate over a multi-dimensional array using ther RecursiveIteratorIterator");
+    }
+
+
+    protected function execute(InputInterface $input, 
+                               OutputInterface $output)
+    {
+		$characters = include '../examples/southparkCharacters.php';
+
+		$output->writeln("Source Array:");
+		print_r($characters);
+
+		$output->writeln("Iterator Output");
+		$iterator = new \RecursiveIteratorIterator(new \RecursiveArrayIterator($characters));
+
+		foreach ($iterator as $key => $value) {
+		    $output->writeln($key . " : " . $value);;
+		}
+
+        $output->writeln("Done");
+        return;
+
+    }
+
+}
+
